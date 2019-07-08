18cc78ee01c0c0b61dbb11ad37aaa9d4cc6c042b
Xiaoguang Liu <vincexgliu@gmail.com>
2019-07-09 03:02:43 +0800

    add sass

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,10 @@ var uglify = require("gulp-uglify");
 var sourcemaps = require("gulp-sourcemaps");
 var buffer = require("vinyl-buffer");
 
+// sass
+const minify = require("gulp-minify-css");
+const sass = require("gulp-sass");
+
 var paths = {
   pages: ["src/*.html"]
 };
@@ -26,35 +30,44 @@ var watchedBrowserify = watchify(
     cache: {},
     packageCache: {}
   })
-    .plugin(tsify)
-    // babel transfer
-    .transform("babelify", {
-      presets: ["es2015"],
-      extensions: [".ts"]
-    })
+  .plugin(tsify)
+  // babel transfer
+  .transform("babelify", {
+    presets: ["es2015"],
+    extensions: [".ts"]
+  })
 );
 
-gulp.task("copy-html", function() {
+gulp.task("copy-html", function () {
   return gulp.src(paths.pages).pipe(gulp.dest("dist"));
 });
 
 function bundle() {
   return (
     watchedBrowserify
-      .bundle()
-      .pipe(source("bundle.js"))
+    .bundle()
+    .pipe(source("bundle.js"))
 
-      // add uglify
-      .pipe(buffer())
-      .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(uglify())
-      .pipe(sourcemaps.write("./"))
+    // add uglify
+    .pipe(buffer())
+    .pipe(sourcemaps.init({
+      loadMaps: true
+    }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write("./"))
 
-      // dist
-      .pipe(gulp.dest("dist"))
+    // dist
+    .pipe(gulp.dest("./dist/js/"))
   );
 }
 
-gulp.task("default", gulp.series(gulp.parallel("copy-html"), bundle));
+gulp.task("sass", function () {
+  return gulp.src("./src/index.scss")
+    .pipe(sass())
+    .pipe(minify())
+    .pipe(gulp.dest("./dist/css/"))
+});
+
+gulp.task("default", gulp.series(gulp.parallel("copy-html", "sass"),  bundle));
 watchedBrowserify.on("update", bundle);
 watchedBrowserify.on("log", fancy_log);
