4235274dbd1a88e551d01e066d7bac0f3e4b7ed2
Xiaoguang Liu <vincexgliu@gmail.com>
2019-07-09 21:21:15 +0800

    image lazy load ok

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -7,21 +7,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const $placeholder: HTMLElement = document.querySelector('.placeholder');
   const $small: HTMLImageElement = document.querySelector('.img-small');
 
-  // // const small: HTMLElement = document.querySelector('.img-small')
+  // const small: HTMLElement = document.querySelector('.img-small')
   
-  // // 1: load small image and show it
-  // const img: HTMLImageElement = new Image();
+  // 1: load small image and show it
+  const img: HTMLImageElement = new Image();
 
-  // img.src = $small.src;
-  // img.onload = function () {
-  //  $small.classList.add('loaded');
-  // };
+  img.src = $small.src;
+  img.onload = function () {
+   $small.classList.add('loaded');
+  };
   
-  // // 2: load large image
-  // const imgLarge: HTMLImageElement = new Image();
-  // imgLarge.src = $placeholder.dataset.large; 
-  // imgLarge.onload = function () {
-  //   imgLarge.classList.add('loaded');
-  // };
-  // $placeholder.appendChild(imgLarge);
+  // 2: load large image
+  const imgLarge: HTMLImageElement = new Image();
+  imgLarge.src = $placeholder.dataset.large; 
+  imgLarge.onload = function () {
+    imgLarge.classList.add('loaded');
+  };
+  $placeholder.appendChild(imgLarge);
 })
