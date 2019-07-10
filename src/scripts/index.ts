8df1ad6b33bd90b8cc89aeeda2b7af88610633a1
Xiaoguang Liu <vincexgliu@gmail.com>
2019-07-10 20:26:07 +0800

    split progresvie load logic from business code

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,41 +1,11 @@
-import * as StackBlur from "../../node_modules/stackblur-canvas/dist/stackblur-es.min.js";
 import "../images/test.jpg";
-// import { $ } from './utils'
-import "../styles/index.scss";
+
+// css import sequence impact cancat css
 import "../styles/reset.css";
+import "../styles/index.scss";
+import "../styles/progressive-load.scss";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const imageNodeList: NodeListOf<HTMLElement> = document.querySelectorAll(
-    ".image-container"
-  );
-  const imageContainer: HTMLElement[] = Array.prototype.slice.apply(
-    imageNodeList
-  );
+import ProgressiveLoad from './progressive-load'
 
-  imageContainer.forEach(e => {
-    const $small: HTMLImageElement = e.querySelector(".img-small");
-    // 这里可以提出进一步封装，调用时输入 image 宽高
-    const addInnerHtml: string = `
-      <div style="padding-bottom: 66.6%;"></div>
-      <canvas></canvas>
-    `;
-    e.innerHTML = e.innerHTML + addInnerHtml;
-    const $canvas: HTMLCanvasElement = e.querySelector("canvas");
-
-    // 1: load small image and show it
-    const img: HTMLImageElement = new Image();
-    img.src = $small.src;
-    img.onload = () => {
-      $small.classList.add("loaded");
-      StackBlur.image($small, $canvas, 30);
-    };
-
-    // 2: load large image
-    const imgLarge: HTMLImageElement = new Image();
-    imgLarge.src = e.dataset.large;
-    imgLarge.onload = () => {
-      imgLarge.classList.add("loaded");
-    };
-    e.appendChild(imgLarge);
-  });
-});
+// progressive load image
+new ProgressiveLoad()
---
+++ src/scripts/progressive-load.ts
@@ -0,0 +1,85 @@
+/**
+ * Progressive load image
+ * 
+ * @param { object } options init options
+ * @return void
+ * 
+ * @date 2019-07-10
+ * @author Xiaoguang Liu <vincexgliu@gmail.com>
+ */
+
+// StackBlur is an open source canvas gauss algorithm blur image
+import * as StackBlur from "../../node_modules/stackblur-canvas/dist/stackblur-es.min.js";
+
+interface IOptions {
+  seletor?: string;
+  width?: number;
+  height?: number;
+  radius?: number;
+}
+
+class ProgressiveLoad {
+  private options: IOptions;
+  private imgRatio: string;
+
+  constructor(options?: IOptions) {
+    const defaultOptions: IOptions = {
+      seletor: ".p-load-image-container",
+      width: 4,
+      height: 3,
+      radius: 30,
+    };
+
+    this.options = { ...defaultOptions, ...options };
+
+    // distruct needn't state type, it's same with right object
+    const { width, height } = this.options;
+    this.imgRatio = ((height / width) * 100).toFixed(2);
+
+    this.init();
+  }
+
+  private init() {
+    document.addEventListener("DOMContentLoaded", () => {
+      const imageNodeList: NodeListOf<HTMLElement> = document.querySelectorAll(
+        this.options.seletor
+      );
+      const imageContainer: HTMLElement[] = Array.prototype.slice.apply(
+        imageNodeList
+      );
+
+      // set each image progessive loading
+      imageContainer.forEach(e => {
+        const $small: HTMLImageElement = e.querySelector("img");
+        // padding-bottom set placeholder, avoid reflow
+        const addInnerHtml: string = `
+          <div style="padding-bottom: ${this.imgRatio}%;"></div>
+          <canvas></canvas>
+        `;
+        e.innerHTML = e.innerHTML + addInnerHtml;
+        const $canvas: HTMLCanvasElement = e.querySelector("canvas");
+
+        // when small image loaded handling it use canvas, this code just avoid small image not loaded
+        // when canvas handle image
+        const img: HTMLImageElement = new Image();
+        img.src = $small.src;
+        img.onload = () => {
+          console.time('a')
+          $small.classList.add("loaded");
+          StackBlur.image($small, $canvas, this.options.radius);
+          console.timeEnd('a')
+        };
+
+        // load large image
+        const imgLarge: HTMLImageElement = new Image();
+        imgLarge.src = e.dataset.large;
+        imgLarge.onload = () => {
+          imgLarge.classList.add("loaded");
+        };
+        e.appendChild(imgLarge);
+      });
+    });
+  }
+}
+
+export default ProgressiveLoad;
