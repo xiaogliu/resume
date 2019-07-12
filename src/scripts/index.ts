e0fd7a807b6d927e899229ad650a68f42d5ff74f
Xiaoguang Liu <vincexgliu@gmail.com>
2019-07-12 18:09:40 +0800

    add nav action

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -4,8 +4,26 @@
 import "../styles/reset.css";
 import "../styles/index.scss";
 import "../styles/progressive-load.scss";
-
-import ProgressiveLoad from './progressive-load'
+import * as utils from "./utils";
+import ProgressiveLoad from "./progressive-load";
 
 // progressive load image
-new ProgressiveLoad()
+new ProgressiveLoad();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const headerEle: HTMLElement = utils.$("header");
+  const headerHeight: number = headerEle.offsetHeight;
+  const navAbout:HTMLElement = utils.$('.nav-about')
+  const navProject:HTMLElement = utils.$('.nav-project')
+
+  window.addEventListener('scroll', () => {
+    console.log(window.scrollY, headerHeight, 2999)
+    if (window.scrollY > headerHeight - 70) {
+      utils.removeClass(navAbout, "nav-active");
+      utils.addClass(navProject, "nav-active")
+    } else {
+      utils.addClass(navAbout, "nav-active");
+      utils.removeClass(navProject, "nav-active");
+    }
+  })
+});
