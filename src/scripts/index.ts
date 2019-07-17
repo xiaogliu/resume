9b4aa9b24180dee2e62e68e6847dfb1fdb61b6f2
liuxiaoguang <vincexgliu@gmail.com>
2019-07-18 01:19:38 +0800

    adjust style

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -8,6 +8,12 @@ import "../styles/p-load.scss";
 import * as utils from "./utils";
 import ProgressiveLoad from "./p-load";
 
+// execute ASAP
+const $bodyEle: HTMLElement = utils.$("body");
+if (utils.isMobile()) {
+  $bodyEle.setAttribute('id', 'mobileVersion')
+}
+
 // progressive load image
 new ProgressiveLoad({
   width: 300,
@@ -16,14 +22,9 @@ new ProgressiveLoad({
 
 document.addEventListener("DOMContentLoaded", () => {
   // const $headerEle: HTMLElement = utils.$("header");
-  const $bodyEle: HTMLElement = utils.$("body");
   // const $headerHeight: number = $headerEle.offsetHeight;
   const $navAbout: HTMLElement = utils.$(".nav-about");
   const $navProject: HTMLElement = utils.$(".nav-project");
-
-  if (utils.isMobile()) {
-    $bodyEle.setAttribute('id', 'mobileVersion')
-  }
 
   window.addEventListener("load", () => {
     const projectTop: number = utils.getElementTop(utils.$("#project"));
