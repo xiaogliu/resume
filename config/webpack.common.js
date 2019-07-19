a8a562518a951f0014e7c67e0b3e7e60bb82a8f5
Xiaoguang Liu <vincexgliu@gmail.com>
2019-07-19 15:18:50 +0800

    add babel assign

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -14,6 +14,7 @@ module.exports = {
         test: /\.ts$/,
         loader: "babel-loader",
         options: {
+          plugins: ["@babel/plugin-transform-object-assign"],
           presets: [
             [
               "@babel/preset-env",
